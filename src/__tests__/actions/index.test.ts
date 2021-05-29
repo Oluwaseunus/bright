3724f4f96e08e57a10576b472f367a3179d51423
Oluwaseun <oluwaseunus@gmail.com>
2021-05-29 18:39:05 +0100

    test: add tests for action creators

---
+++ src/__tests__/actions/index.test.ts
@@ -0,0 +1,38 @@
+import { addShape, removeShape } from '../../store/actions/shapes';
+import { addColour, removeColour } from '../../store/actions/colours';
+
+describe('Actions', () => {
+  describe('Colour Actions', () => {
+    const colour = 'Red';
+    let expectedAction = {
+      type: 'add_colour',
+      colour,
+    };
+
+    it('should create an action to add a colour', () => {
+      expect(addColour(colour)).toEqual(expectedAction);
+    });
+
+    it('should create an action to remove a colour', () => {
+      expectedAction.type = 'remove_colour';
+      expect(removeColour(colour)).toEqual(expectedAction);
+    });
+  });
+
+  describe('Shape Actions', () => {
+    const shape = 'Oval';
+    let expectedAction = {
+      type: 'add_shape',
+      shape,
+    };
+
+    it('should create an action to add a shape', () => {
+      expect(addShape(shape)).toEqual(expectedAction);
+    });
+
+    it('should create an action to remove a shape', () => {
+      expectedAction.type = 'remove_shape';
+      expect(removeShape(shape)).toEqual(expectedAction);
+    });
+  });
+});
